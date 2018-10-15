1aaae2a8796d48d75266c0d788c23d064ab97625
ExoBlod <envyxwrath@outlook.com.pe>
2018-10-15 15:27:16 -0500

    Cambio en catalogfragment

--- Proyecto-Furukawa-master/app/src/main/java/com/glitched/utp/furukawa/CatalogFragment.java
+++ Proyecto-Furukawa-master/app/src/main/java/com/glitched/utp/furukawa/CatalogFragment.java
@@ -3,20 +3,31 @@ package com.glitched.utp.furukawa;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class CatalogFragment extends Fragment {
     public CatalogFragment() {
-        // Required empty public constructor
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        Button button1 =  view.findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllProductsFragment fr=new AllProductsFragment();
+                FragmentTransaction fragmentTransaction= getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container,fr);
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 }
--- Proyecto-Furukawa-master/app/src/main/java/com/glitched/utp/furukawa/QuoteFragment.java
+++ Proyecto-Furukawa-master/app/src/main/java/com/glitched/utp/furukawa/QuoteFragment.java
@@ -19,7 +19,7 @@ public class QuoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quote, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
     }
 }
